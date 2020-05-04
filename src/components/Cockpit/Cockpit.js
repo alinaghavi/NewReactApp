d93475ab218a41161cfc8f37d4b4758d5391cd32
naghavi.ali <naghavi.ali@gmail.com>
2020-05-04 11:48:22 +0430

    Some changes added

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,28 +1,52 @@
-import React from 'react';
-import classes from "./Cockpit.module.css";
+import React, { useEffect } from 'react';
 
-const cockpit = (props) => {
-    let btnClass = '';
-    if (props.showPersons) {
-        btnClass = classes.Red;
-    }
+import classes from './Cockpit.module.css';
 
-    const assignedClasses = [];
-    if (props.persons.length <= 2) {
-        assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (props.persons.length <= 1) {
-        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>
-                Toggle Persons
-            </button>
-        </div>
-    );
+const Cockpit = props => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...
+    const timer = setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
+
+  // useEffect();
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <h1>Counter is : {props.counter}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
 };
 
-export default cockpit;
+export default React.memo(Cockpit);
